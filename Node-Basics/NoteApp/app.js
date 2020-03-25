0a582be36f040c28551f463b36cc26a2bc85dbc4
SibilJoeKurian <Sibil.kurian@gmail.com>
2020-03-25 10:56:09 -0400

    file system

--- Node-Basics/NoteApp/app.js
+++ Node-Basics/NoteApp/app.js
@@ -1,3 +1,43 @@
-const chalk=require('chalk')
+const yargs=require('yargs')
 
-console.log(chalk.red('hello world'))
+yargs.command({
+    command:'add',
+    describe:'add a note',
+    handler:function(){
+        console.log('In add command')
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler:function(){
+        console.log('remove a note');
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'list a note',
+    handler: function(){
+        console.log('list a note')
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'read a note',
+    builder:{
+        title:{
+            describe:'read a note title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(){
+        console.log('read a note')
+    }
+})
+
+yargs.parse()
+
